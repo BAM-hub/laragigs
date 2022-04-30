a04865ed37562a486575e9a8ab346aae95683398
BAM-hub <bsharamin12@gmail.com>
2022-05-01 02:39:43 +0300

    on submit fail old field value reserved now and can upload image

--- resources/views/Listings/create.blade.php
+++ resources/views/Listings/create.blade.php
@@ -8,7 +8,7 @@
         <p class="mb-4">Post a gig to find a developer</p>
     </header>
 
-    <form method="POST" action="/listings">
+    <form method="POST" action="/listings" enctype="multipart/form-data">
         @csrf
         <div class="mb-6">
             <label
@@ -20,6 +20,7 @@
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="company"
+                value="{{old('company')}}"
             />
             @error('company')
               <p class="text-red-500 text-xs mt-1">
@@ -37,6 +38,7 @@
                 class="border border-gray-200 rounded p-2 w-full"
                 name="title"
                 placeholder="Example: Senior Laravel Developer"
+                value="{{old('title')}}"
             />
             @error('title')
             <p class="text-red-500 text-xs mt-1">
@@ -56,6 +58,7 @@
                 class="border border-gray-200 rounded p-2 w-full"
                 name="location"
                 placeholder="Example: Remote, Boston MA, etc"
+                value="{{old('location')}}"
             />
             @error('location')
             <p class="text-red-500 text-xs mt-1">
@@ -72,6 +75,7 @@
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="email"
+                value="{{old('email')}}"
             />
             @error('email')
             <p class="text-red-500 text-xs mt-1">
@@ -91,6 +95,7 @@
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="website"
+                value="{{old('website')}}"
             />
             @error('website')
             <p class="text-red-500 text-xs mt-1">
@@ -108,6 +113,7 @@
                 class="border border-gray-200 rounded p-2 w-full"
                 name="tags"
                 placeholder="Example: Laravel, Backend, Postgres, etc"
+                value="{{old('tags')}}"
             />
             @error('tags')
             <p class="text-red-500 text-xs mt-1">
@@ -116,7 +122,7 @@
           @enderror
         </div>
 
-        {{-- <div class="mb-6">
+        <div class="mb-6">
             <label for="logo" class="inline-block text-lg mb-2">
                 Company Logo
             </label>
@@ -125,7 +131,12 @@
                 class="border border-gray-200 rounded p-2 w-full"
                 name="logo"
             />
-        </div> --}}
+        </div>
+        @error('logo')
+        <p class="text-red-500 text-xs mt-1">
+            {{ $message }}
+        </p>
+      @enderror
 
         <div class="mb-6">
             <label
@@ -139,7 +150,10 @@
                 name="description"
                 rows="10"
                 placeholder="Include tasks, requirements, salary, etc"
-            ></textarea>
+            >
+                {{old('description')}}
+        
+            </textarea>
             @error('description')
             <p class="text-red-500 text-xs mt-1">
                 {{ $message }}
