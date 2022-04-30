daf2d018ab39f3d59caa216f430f4c6bfb2cc27c
BAM-hub <bsharamin12@gmail.com>
2022-05-01 00:53:14 +0300

    added filttering for the listings based on the query and did auth for new gigs

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
     // show all listings
     public function index() {
         return view('listings.index', [
-          'listings' => Listing::all()
+          'listings' => Listing::latest()->filter(
+            request(['tag', 'search'])
+            )->get()
         ]);
     }
     
@@ -22,4 +25,29 @@ class ListingController extends Controller
         ]); 
     }
 
+    //show create form
+    public function create() {
+      return view('listings.create');
+    }
+
+    // store listing data
+    public function store(Request $request) {
+      $formFields = $request->validate([
+        'title' => 'required',
+        'company' => ['required', Rule::unique(
+            'listings',
+            'company'
+            )
+        ],
+        'location' => 'required',
+        'website' => 'required',
+        'email' => ['required', 'email'],
+        'tags' => 'required',
+        'description' => 'required'
+      ]);
+
+      Listing::create($formFields);
+
+      return redirect('/');
+    }
 }
