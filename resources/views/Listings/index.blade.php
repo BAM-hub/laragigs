8d0098ce4163f03695fe52579c43fc0597ea0975
BAM-hub <bsharamin12@gmail.com>
2022-05-01 02:40:10 +0300

    added pagination and fixed the grid container

--- resources/views/Listings/index.blade.php
+++ resources/views/Listings/index.blade.php
@@ -2,7 +2,7 @@
 @include('partials._hero')
 @include('partials._search')
 
-
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
   @unless (count($listings) != 0)
   <p>No Listings Found</p>
   @endunless
@@ -10,4 +10,8 @@
   @foreach($listings as $listing)
     <x-listing-card :listing="$listing" />
   @endforeach
+</div>
+<div class="mt-6 p-4">
+  {{ $listings->links() }}
+</div>
 </x-layout>
