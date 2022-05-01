799bbf1c33ea6b7a12ea1b3583d44d976079b37a
BAM-hub <bsharamin12@gmail.com>
2022-05-02 00:31:33 +0300

    functionality for the new routes is added

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -59,4 +59,37 @@ class ListingController extends Controller
         'Listing Created successfully!'
       );
     }
+    
+    // show edit form
+    public function edit(Listing $listing) {
+      return view('listings.edit', ['listing' => $listing]);
+    }
+
+     // update listing data
+    public function update(Request $request, Listing $listing) {
+      
+      $formFields = $request->validate([
+        'title' => 'required',
+        'company' => 'required',
+        'location' => 'required',
+        'website' => 'required',
+        'email' => ['required', 'email'],
+        'tags' => 'required',
+        'description' => 'required'
+      ]);
+
+      if($request->hasFile('logo')) {
+        $formFields['logo'] = $request->file('logo')->store(
+          'logos', 'public'
+        );
+      }
+      $listing->update($formFields);
+      return back()->with('message', 'Listing Updated successfully');
+    }
+
+    // delete listing
+    public function destroy(Listing $listing) {
+      $listing->delete();
+      return redirect('/')->with('message', 'Listing Deleted successfully');
+    }
 }
