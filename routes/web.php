c0ca0bb5c480f2b4ddbe4e63a2c61deb16f91985
BAM-hub <bsharamin12@gmail.com>
2022-05-02 00:30:47 +0300

    the show update and delete listing routes are added

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
-use Illuminate\Http\Request;
-use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
-use Illuminate\View\View;
-use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,6 +25,14 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 // store listing data
 Route::post('/listings', [ListingController::class, 'store']);
 
+// show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// update listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// delete listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 //Single Listing
 @Route::get('/listings/{listing}', [ListingController::class, 'show']);
